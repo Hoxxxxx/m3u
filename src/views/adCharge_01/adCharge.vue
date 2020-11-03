9eaa86e195f5491bf8c5f41e43f992b16b45d4ab
侯鑫 <houxin@qq.com>
2020-11-03 16:21:41 +0800

    菜单栏样式

--- src/views/adCharge_01/adCharge.vue
+++ src/views/adCharge_01/adCharge.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="workSpace">
-
     <!-- 表单区域 -->
     <el-card class="formContent">
       <el-tabs v-model="activeTab" @tab-click="handleClick">
         <el-tab-pane name="firTab">
-          <span slot="label"> 表单</span>
+          <div slot="label" class="potBox">
+            <span
+              class="pot"
+              :class="activeTab == 'firTab' ? 'potActive' : ''"
+            ></span>
+            <span>表单</span>
+          </div>
           1111
         </el-tab-pane>
         <el-tab-pane label="流程进度" name="secTab">
-          <span slot="label"> 流程进度</span>
+          <div slot="label" class="potBox">
+            <span
+              class="pot"
+              :class="activeTab == 'secTab' ? 'potActive' : ''"
+            ></span>
+            <span> 流程进度</span>
+          </div>
           1111112222
         </el-tab-pane>
       </el-tabs>
     </el-card>
     <!-- 附件区域 -->
-    <el-card class="uploadContent">
-      
-    </el-card>
-
+    <el-card class="uploadContent"> </el-card>
   </div>
 </template>
 
@@ -26,24 +34,21 @@
 // import FixedBtns from "@/components/fixedBtns";
 
 export default {
-  components: {
-  },
+  components: {},
   data() {
     return {
-      activeTab: 'firTab',
+      activeTab: "firTab",
     };
   },
-  created() {
-  },
+  created() {},
   methods: {
     handleClick() {
-      console.log(this.activeTab)
-    }
+      console.log(this.activeTab);
+    },
   },
 };
 </script>
 
 <style lang="less" scoped>
 @import "../../assets/style/public.less";
-
 </style>
