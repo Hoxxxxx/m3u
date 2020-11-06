79986b95906db07afdb719c7b919328c9ad82e7d
侯鑫 <houxin@qq.com>
2020-11-06 18:44:58 +0800

    添加弹窗

--- src/views/works/travel/newProcess.vue
+++ src/views/works/travel/newProcess.vue
@@ -6,7 +6,9 @@
         <el-button type="primary" @click="$router.push('/')"
           >回到首页</el-button
         >
-        <el-button type="primary" class="save" @click="save">保存</el-button>
+        <el-button type="primary" class="save" @click="addNewFlow"
+          >保存</el-button
+        >
         <el-button type="primary" class="next" @click="$router.push('/apply')"
           >下一步</el-button
         >
@@ -37,20 +39,8 @@
                   {{ tableData.oaa03 }}
                 </div>
                 <div class="titlebox">申请人</div>
-                <div class="infobox selectbox">
-                  <el-select
-                    v-model="tableData.oaa04"
-                    class="select"
-                    placeholder="请选择申请人"
-                  >
-                    <el-option
-                      v-for="(item, index) in fixedData.applys"
-                      :key="index"
-                      :label="item.gen02"
-                      :value="item.gen01"
-                    >
-                    </el-option>
-                  </el-select>
+                <div class="infobox selectbox" >
+                  <div class="selector" @click="selectDialog('SQR')">{{tableData.oaa04}}</div>
                 </div>
                 <div class="titlebox">联系电话</div>
                 <div class="infobox">
@@ -571,7 +561,7 @@
                   >
                     <template slot-scope="scope">
                       <div>
-                        <el-select
+                        <!-- <el-select
                           v-model="scope.row.oac01"
                           placeholder="请选择会计科目"
                         >
@@ -582,7 +572,8 @@
                             :value="item.aag01"
                           >
                           </el-option>
-                        </el-select>
+                        </el-select> -->
+                        <div class="selector selectBorder" @click="selectDialog('KJKM',scope.$index)">{{scope.row.oac01}}</div>
                       </div>
                     </template>
                   </el-table-column>
@@ -594,7 +585,7 @@
                   >
                     <template slot-scope="scope">
                       <div>
-                        <el-select
+                        <!-- <el-select
                           v-model="scope.row.oac04"
                           placeholder="请选择项目"
                         >
@@ -605,7 +596,8 @@
                             :value="item.pja01"
                           >
                           </el-option>
-                        </el-select>
+                        </el-select> -->
+                        <div class="selector selectBorder" @click="selectDialog('XM',scope.$index)">{{scope.row.oac04}}</div>
                       </div>
                     </template>
                   </el-table-column>
@@ -617,7 +609,7 @@
                   >
                     <template slot-scope="scope">
                       <div>
-                        <el-select
+                        <!-- <el-select
                           v-model="scope.row.oac05"
                           placeholder="请选择项目WBS"
                         >
@@ -628,7 +620,8 @@
                             :value="item.pjb02"
                           >
                           </el-option>
-                        </el-select>
+                        </el-select> -->
+                        <div class="selector selectBorder" @click="selectDialog('WBS',scope.$index)">{{scope.row.oac05}}</div>
                       </div>
                     </template>
                   </el-table-column>
@@ -819,7 +812,7 @@
                     <template slot-scope="scope">
                       <div>
                         <el-input
-                          v-model="scope.row.apb28"
+                          v-model="scope.row.apb35"
                           placeholder="请输入借款总金额"
                         ></el-input>
                       </div>
@@ -834,7 +827,7 @@
                     <template slot-scope="scope">
                       <div>
                         <el-input
-                          v-model="scope.row.apb28"
+                          v-model="scope.row.oad02"
                           placeholder="请输入还款金额"
                         ></el-input>
                       </div>
@@ -849,7 +842,7 @@
                     <template slot-scope="scope">
                       <div>
                         <el-input
-                          v-model="scope.row.apb28"
+                          v-model="scope.row.apb25"
                           placeholder="请输入凭证号"
                         ></el-input>
                       </div>
@@ -898,7 +891,8 @@
           :before-remove="beforeRemove"
           multiple
           :on-exceed="handleExceed"
-          :file-list="fileList">
+          :file-list="fileList"
+        >
           <el-button size="small" type="primary">点击上传</el-button>
         </el-upload>
       </div>
@@ -925,11 +919,28 @@
         </el-timeline>
       </div>
     </el-card>
+    <!-- 数据选择弹出框 -->
+    <SelectData
+      :isLoading="dataSelect.selectLoading"
+      :dialogTitle.sync="dataSelect.dialogTitle"
+      :dialogVisible.sync="dataSelect.dialogVisible"
+      :headList.sync="dataSelect.headList"
+      :bodyData.sync="dataSelect.bodyData"
+      :choosedData="dataSelect.choosedData"
+      :editType.sync="dataSelect.editType"
+      :searchApi="dataSelect.searchApi"
+      :filter="dataSelect.filter"
+      :keyMsg="dataSelect.keyMsg"
+      @selectSure="selectSure"
+      @selectCancel="selectCancel"
+    ></SelectData>
   </div>
 </template>
 
 <script>
+import SelectData from "@/components/selectData";
 import { dateFmt } from "@/utils/utils.js";
+import { addFlow } from "@/api/process_new";
 import {
   gensList,
   azisList,
@@ -940,7 +951,7 @@ import {
 } from "@/api/basic.js";
 
 export default {
-  components: {},
+  components: {SelectData},
   data() {
     return {
       activeTab: "firTab",
@@ -972,8 +983,6 @@ export default {
         oad: [], // 冲销信息
       },
       fixedData: {
-        // 申请人列表
-        applys: [],
         // 币种列表
         cointypes: [],
         //支付方式
@@ -1045,7 +1054,54 @@ export default {
       addParams: {
         from_data: {},
         annexurlid: [],
-        tplid: 8936
+        tplid: 8943,
+      },
+
+      rowIndex:"",//当前点击的行数
+      //数据选择弹出框
+      dataSelect: {
+        editType:"entry",
+        selectLoading:false,
+        cur_input: "", // 当前点击的输入框
+        dialogTitle: "数据选择", //当前弹框的title
+        dialogVisible: false, //控制显示隐藏弹框
+        headList: [], //表头
+        bodyData: [], //表格数据
+        choosedData: [], //选中的数据
+        searchApi: "", //搜索框的接口地址
+        filter: [], //筛选条件
+        keyMsg: [], //需要显示在顶部的数据
+      },
+      // 弹出框表头数据
+      tableHead: {
+        // 申请人
+        head_SQR: [
+          { name: "gen01", title: "员工编号" },
+          { name: "gen02", title: "员工名称" },
+          { name: "gen03", title: "所属部门编号" },
+        ],
+        head_KJKM: [
+          { name: "aag01", title: "科目编号" },
+          { name: "aag02", title: "科目名称" },
+          { name: "aag03", title: "科目性质" },
+          { name: "aag04", title: "资产损益别" },
+          { name: "aag07", title: "统制明细别" },
+          { name: "aag13", title: "额外名称" },
+          { name: "aag24", title: "科目层级" },
+        ],
+        head_XM: [
+          { name: "pja01", title: "项目编号" },
+          { name: "pja02", title: "项目名称" },
+          { name: "pja08", title: "项目负责人" },
+          { name: "pja09", title: "负责部门" },
+          { name: "pja13", title: "项目预计总额" },
+        ],
+        head_WBS: [
+          { name: "pjb02", title: "WBS编号" },
+          { name: "pjb03", title: "WBS名称" },
+          { name: "pjb01", title: "项目编号" },
+          { name: "pja02", title: "项目名称" },
+        ],
       },
     };
   },
@@ -1053,12 +1109,8 @@ export default {
     this.addRow1();
     this.addRow2();
     this.addRow3();
-    this.getGen(); //申请人列表
     this.getAzi(); //币种列表
     this.getPma(); //支付方式
-    this.getPjb(); //wbs
-    this.getAags(); //会计科目
-    this.getPjas(); //项目
   },
   methods: {
     // 获取当前时间 格式:yyyy-MM-dd HH:MM:SS
@@ -1100,35 +1152,39 @@ export default {
     handleSuccess(response, file, fileList) {
       this.addParams.annexurlid.push({
         filename: response.data.filename,
-        fileaddr: response.data.path
-      })
+        fileaddr: response.data.path,
+      });
     },
     // 移除上传项
     handleRemove(file, fileList) {
-      this.addParams.annexurlid.forEach( (item, index) => {
+      this.addParams.annexurlid.forEach((item, index) => {
         if (item.filename == file.name) {
-          this.addParams.annexurlid.splice( index, 1 )
+          this.addParams.annexurlid.splice(index, 1);
         }
-      })
+      });
     },
     handlePreview(file) {
       console.log(file);
     },
     handleExceed(files, fileList) {
-      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+      this.$message.warning(
+        `当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${
+          files.length + fileList.length
+        } 个文件`
+      );
     },
     beforeRemove(file, fileList) {
-      return this.$confirm(`确定移除 ${ file.name }？`);
+      return this.$confirm(`确定移除 ${file.name}？`);
     },
     // *******************************************
     // ****************其他操作*******************
     // 新增表单
     addNewFlow() {
-      this.addParams.from_data = this.tableData
-      addFlow(this.addParams)
-      .then( result => {
-        console.log(result)
-      })
+      this.addParams.from_data = this.tableData;
+      console.log(this.addParams);
+      addFlow(this.addParams).then((result) => {
+        console.log(result);
+      });
     },
     // *******************************************
     // 表格部分
@@ -1213,17 +1269,7 @@ export default {
         }
       });
     },
-    // 获取基础数据
-    // 申请人列表
-    getGen() {
-      gensList().then((res) => {
-        if (res.status == 200) {
-          this.fixedData.applys = res.data;
-        } else {
-          this.$message.error(res.error);
-        }
-      });
-    },
+    // 获取基础数据*******
     // 币种列表
     getAzi() {
       azisList().then((res) => {
@@ -1244,35 +1290,79 @@ export default {
         }
       });
     },
-    // wbs列表
-    getPjb() {
-      pjbsList().then((res) => {
-        if (res.status == 200) {
-          this.fixedData.options_03 = res.data;
-        } else {
-          this.$message.error(res.error);
-        }
-      });
+    // ******************
+    // 数据选择
+    selectDialog(type,rowIndex) {
+      this.rowIndex = rowIndex;
+      this.dataSelect.dialogVisible = true;
+      this.dataSelect.cur_input = type;
+      this.dataSelect.choosedData = [];
+      switch (type) {
+        case "SQR":
+          let filter_SQR = [{ label: "", model_key_search: "keyword" }];
+          this.dataSelect.filter = filter_SQR;
+          this.dataSelect.searchApi = "meta/gens";
+          this.selectLoading = false;
+          this.dataSelect.headList = this.tableHead.head_SQR;
+          this.dataSelect.dialogTitle = "员工列表";
+          break;
+        case "KJKM":
+          let filter_KJKM = [{ label: "", model_key_search: "keyword" }];
+          this.dataSelect.filter = filter_KJKM;
+          this.dataSelect.searchApi = "meta/aags";
+          this.selectLoading = false;
+          this.dataSelect.headList = this.tableHead.head_KJKM;
+          this.dataSelect.dialogTitle = "会计科目";
+          break;
+        case "XM":
+          let filter_XM = [{ label: "", model_key_search: "keyword" }];
+          this.dataSelect.filter = filter_XM;
+          this.dataSelect.searchApi = "meta/pjas";
+          this.selectLoading = false;
+          this.dataSelect.headList = this.tableHead.head_XM;
+          this.dataSelect.dialogTitle = "项目";
+          break;
+        case "WBS":
+          let filter_WBS = [{ label: "", model_key_search: "keyword" }];
+          this.dataSelect.filter = filter_WBS;
+          this.dataSelect.searchApi = "meta/pjbs";
+          this.selectLoading = false;
+          this.dataSelect.headList = this.tableHead.head_WBS;
+          this.dataSelect.dialogTitle = "WBS列表";
+          break;
+        default:
+          return;
+          break;
+      }
     },
-    //会计科目
-    getAags() {
-      aagsList().then((res) => {
-        if (res.status == 200) {
-          this.fixedData.options_01 = res.data;
-        } else {
-          this.$message.error(res.error);
-        }
-      });
+    selectCancel(val) {
+      this.dataSelect.dialogVisible = false;
+      this.dataSelect.bodyData = val;
+      this.dataSelect.choosedData = val;
     },
-    // 项目列表
-    getPjas() {
-      pjasList().then((res) => {
-        if (res.status == 200) {
-          this.fixedData.options_02 = res.data;
-        } else {
-          this.$message.error(res.error);
+    selectSure(val) {
+      this.dataSelect.dialogVisible = false;
+      this.dataSelect.bodyData = [];
+      this.dataSelect.choosedData = val;
+      if (val.length > 0) {
+        switch (this.dataSelect.cur_input) {
+          case "SQR":
+            this.tableData.oaa04 = val[0].gen01;
+            break;
+          case "KJKM":
+            this.tableData.oac[this.rowIndex].oac01 = val[0].aag01
+            break;
+          case "XM":
+            this.tableData.oac[this.rowIndex].oac04 = val[0].pja01
+            break;
+          case "WBS":
+            this.tableData.oac[this.rowIndex].oac05 = val[0].pjb02
+            break;
+          default:
+            return;
+            break;
         }
-      });
+      }
     },
   },
 };
