64cae5aa8dde10bf95767db53ec1de3cf04dbe58
Limq <m18281696638@163.com>
2020-11-10 17:06:25 +0800

    大写金额

--- src/views/works/borrow/check.vue
+++ src/views/works/borrow/check.vue
@@ -70,6 +70,12 @@
                 <div class="infobox longbox  selectbox">{{tableData.oaa12_show}}</div>
               </div>
               <div class="form_line">
+                <div class="titlebox">折合汇率金额</div>
+                <div class="infobox middlebox disabledbox">{{exchange}}</div>
+                <div class="titlebox">折合汇率金额大写</div>
+                <div class="infobox middlebox disabledbox last_row">{{exchange_Cap}}</div>
+              </div>
+              <div class="form_line">
                 <div class="titlebox">出差单</div>
                 <div class="infobox longbox">{{tableData.oaa17_show}}</div>
               </div>
@@ -163,6 +169,8 @@ export default {
       workname: '出差借款申请',
       activeTab: "firTab",
       tableData: {},
+      exchange: '',
+      exchange_Cap: '',
       fixedData: {
         selectLoading: true,
         // 申请人列表
@@ -209,10 +217,70 @@ export default {
               })
             })
           }
+          this.getExchangeRate()
         }else{
           this.$message.error('获取流程信息失败：', res.error.message);
         }
       })
+    },
+    // 计算折合汇率
+    getExchangeRate() {
+      this.exchange = Number(this.tableData.oaa07) * Number(this.tableData.oaa08)
+      this.exchange = this.exchange.toFixed(2)
+      this.NumberToChinese(this.exchange)
+    },
+    //阿拉伯数字转换函数
+    toDx(n) {
+      switch (n) {
+          case "0":
+              return "零";
+          case "1":
+              return "壹";
+          case "2":
+              return "贰";
+          case "3":
+              return "叁";
+          case "4":
+              return "肆";
+          case "5":
+              return "伍";
+          case "6":
+              return "陆";
+          case "7":
+              return "柒";
+          case "8":
+              return "捌";
+          case "9":
+              return "玖";
+      }
+    },
+    // 转大写
+    NumberToChinese(m){
+      m *= 100;
+      m += "";
+      var length = m.length;
+
+      var result = "";
+      for (var i = 0; i < length; i++) {
+          if (i == 2) {
+            result = "元" + result;
+          } else if (i == 6) {
+            result = "万" + result;
+          } else if (i == 10) {
+            result = "亿" + result;
+          }
+          if (m.charAt(length - i - 1) == 0) {
+              if (i != 0 && i != 1) {
+                  if (result.charAt(0) != '零' && result.charAt(0) != '元' && result.charAt(0) != '万') {
+                      result = "零" + result;
+                  }
+              }
+              continue;
+          }
+          result = this.toDx(m.charAt(length - i - 1)) + this.unit[this.unit.length - i - 1] + result;
+      }
+      result += result.charAt(result.length - 1) == '元' ? "整" : "";
+      this.exchange_Cap = result;
     },
     // *******************************************
     // ***************附件上传******************
--- src/views/works/borrow/newApproval.vue
+++ src/views/works/borrow/newApproval.vue
@@ -76,6 +76,12 @@
                 <div class="infobox longbox  selectbox">{{tableData.oaa12_show}}</div>
               </div>
               <div class="form_line">
+                <div class="titlebox">折合汇率金额</div>
+                <div class="infobox middlebox disabledbox">{{exchange}}</div>
+                <div class="titlebox">折合汇率金额大写</div>
+                <div class="infobox middlebox disabledbox last_row">{{exchange_Cap}}</div>
+              </div>
+              <div class="form_line">
                 <div class="titlebox">出差单</div>
                 <div class="infobox longbox">{{tableData.oaa17_show}}</div>
               </div>
@@ -234,6 +240,7 @@ export default {
               })
             })
           }
+          this.getExchangeRate()
         }else{
           this.$message.error('获取流程信息失败：' + res.error.message);
         }
@@ -337,6 +344,65 @@ export default {
     },
     // ******************************************
     // ****************其他操作*******************
+    // 计算折合汇率
+    getExchangeRate() {
+      this.exchange = Number(this.tableData.oaa07) * Number(this.tableData.oaa08)
+      this.exchange = this.exchange.toFixed(2)
+      this.NumberToChinese(this.exchange)
+    },
+    //阿拉伯数字转换函数
+    toDx(n) {
+      switch (n) {
+          case "0":
+              return "零";
+          case "1":
+              return "壹";
+          case "2":
+              return "贰";
+          case "3":
+              return "叁";
+          case "4":
+              return "肆";
+          case "5":
+              return "伍";
+          case "6":
+              return "陆";
+          case "7":
+              return "柒";
+          case "8":
+              return "捌";
+          case "9":
+              return "玖";
+      }
+    },
+    // 转大写
+    NumberToChinese(m){
+      m *= 100;
+      m += "";
+      var length = m.length;
+
+      var result = "";
+      for (var i = 0; i < length; i++) {
+          if (i == 2) {
+            result = "元" + result;
+          } else if (i == 6) {
+            result = "万" + result;
+          } else if (i == 10) {
+            result = "亿" + result;
+          }
+          if (m.charAt(length - i - 1) == 0) {
+              if (i != 0 && i != 1) {
+                  if (result.charAt(0) != '零' && result.charAt(0) != '元' && result.charAt(0) != '万') {
+                      result = "零" + result;
+                  }
+              }
+              continue;
+          }
+          result = this.toDx(m.charAt(length - i - 1)) + this.unit[this.unit.length - i - 1] + result;
+      }
+      result += result.charAt(result.length - 1) == '元' ? "整" : "";
+      this.exchange_Cap = result;
+    },
     // 新增表单
     addNewFlow() {
       this.addParams.from_data = this.tableData
