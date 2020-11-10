fdedb1c9fce45749e0626e24ba18ecd43c941a41
侯鑫 <houxin@qq.com>
2020-11-10 16:08:24 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -17,12 +17,17 @@ module.exports = {
     extract: false
   },
   configureWebpack: {
-    externals: {
-      'vue': 'Vue',
-      'element-ui': 'ELEMENT',
-      'vue-router': 'VueRouter',
-      'vuex': 'Vuex',
-      'axios': 'axios'
+    //警告 webpack 的性能提示
+    performance: {
+      hints: 'warning',
+      //入口起点的最大体积
+      maxEntrypointSize: 50000000,
+      //生成文件的最大体积
+      maxAssetSize: 30000000,
+      //只给出 js 文件的性能提示
+      assetFilter: function (assetFilename) {
+        return assetFilename.endsWith('.js');
+      }
     }
-  }
+  },
 }
