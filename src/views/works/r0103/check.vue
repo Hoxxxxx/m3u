5697d58e5672cd639f60214065901166e299ad04
Limq <m18281696638@163.com>
2020-11-19 17:44:12 +0800

    增补loading

--- src/views/works/r0103/check.vue
+++ src/views/works/r0103/check.vue
@@ -362,11 +362,12 @@
 
 <script>
 import { workflowsList, } from "@/api/process_new.js"
-import { number_chinese } from "@/utils/utils.js";
+import { number_chinese, OpenLoading } from "@/utils/utils.js";
 
 export default {
   data() {
     return {
+      overloading: '', //加载定时器
       workid: '',
       workname: '其他费用报销单',
       activeTab: "firTab",
@@ -434,18 +435,14 @@ export default {
     },
     // ***********获取流程信息************
     getworkflows(){
-      const loading = this.$loading({
-        lock: true,
-        text: "Loading",
-        spinner: "el-icon-loading",
-        background: "rgba(0, 0, 0, 0.7)",
-      });
+      const loading = OpenLoading(this, 1)
       const params = {
         workid: this.workid
       }
       workflowsList(params).then(res=>{
         if(res.status == 200){
           loading.close()
+          clearTimeout(this.overloading)
           this.tableData = res.data.workclass_info.from_data
           this.workname = res.data.workclass_info.title
           this.workclass_perflow = res.data.workclass_perflow
@@ -460,6 +457,7 @@ export default {
           }
         }else{
           loading.close()
+          clearTimeout(this.overloading)
           this.$message.error('获取流程信息失败：', res.error.message);
         }
       })
--- src/views/works/r0103/newProcess.vue
+++ src/views/works/r0103/newProcess.vue
@@ -3,12 +3,8 @@
     <!-- 表单区域 -->
     <el-card class="formContent">
       <div class="btnBox" v-if="activeTab == 'firTab'">
-        <el-button type="primary" class="save" @click="addNewFlow()"
-          >保存</el-button
-        >
-        <el-button type="primary" class="next" @click="nextStep()"
-          >下一步</el-button
-        >
+        <el-button type="primary" class="save" @click="addNewFlow('add')">保存</el-button>
+        <el-button type="primary" class="next" @click="addNewFlow('next')">下一步</el-button>
       </div>
       <el-tabs v-model="activeTab" @tab-click="handleClick">
         <el-tab-pane name="firTab">
@@ -805,7 +801,23 @@ export default {
             this.workid = result.data.workid;
             this.tableData.oaa01 = result.data.oaa01;
             this.tableData.oaa02 = result.data.oaa02;
-            this.$message.success("保存成功！");
+            if (type == 'add') {
+              this.$message.success("保存成功！");
+            } else if (type == 'next') {
+              this.$router.push(
+                {
+                  path:'/apply',
+                  query: {
+                    url_type: 'r103',
+                    workName:this.workName,
+                    workid: this.workid,
+                    workName: this.workName,
+                    oaa01: this.tableData.oaa01,
+                    oaa02: this.tableData.oaa02
+                  }
+                }
+              )
+            }
           } else {
             this.$message.error("保存失败：" + result.error.message);
           }
@@ -814,31 +826,28 @@ export default {
         this.addParams.workid = this.workid;
         editFlow(this.addParams).then((result) => {
           if (result.status == 200) {
-            this.$message.success("保存成功！");
+            if (type == 'add') {
+              this.$message.success("保存成功！");
+            } else if (type == 'next') {
+              this.$router.push(
+                {
+                  path:'/apply',
+                  query: {
+                    url_type: 'r103',
+                    workName:this.workName,
+                    workid: this.workid,
+                    workName: this.workName,
+                    oaa01: this.tableData.oaa01,
+                    oaa02: this.tableData.oaa02
+                  }
+                }
+              )
+            }
           } else {
             this.$message.error("保存失败：" + result.error.message);
           }
         });
       }
-    },
-    // 下一步
-    nextStep() {
-      this.addNewFlow();
-      this.$nextTick(() => {
-        if (this.workid !== "") {
-          this.$router.push({
-            path: "/apply",
-            query: {
-              url_type: "otherFees",
-              workName: this.workName,
-              workid: this.workid,
-              workname: this.workname,
-              oaa01: this.tableData.oaa01,
-              oaa02: this.tableData.oaa02,
-            },
-          });
-        }
-      });
     },
     // *******************************************
     // 表格部分
