db33a85884739e627a916a870eb3ddbde20e20ac
侯鑫 <houxin@qq.com>
2020-11-23 10:16:19 +0800

    修改字段

--- src/views/works/r0102/newProcess.vue
+++ src/views/works/r0102/newProcess.vue
@@ -645,7 +645,7 @@
                     </template>
                   </el-table-column>
                   <el-table-column
-                    prop="oad04"
+                    prop="oad04_show"
                     label="借款人"
                     min-width="110px"
                     align="center"
@@ -653,7 +653,7 @@
                     <template slot-scope="scope">
                       <div>
                         <el-input
-                          v-model="scope.row.oad04"
+                          v-model="scope.row.oad04_show"
                           placeholder=""
                           disabled
                         ></el-input>
@@ -1417,6 +1417,7 @@ export default {
               this.$set(item,'oad02','')
               this.$set(item,'oad03',item.date)
               this.$set(item,'oad04',item.rid)
+              this.$set(item,'oad04_show',item.rname)
               this.$set(item,'oad05',item.original_amount)
               this.$set(item,'oad06',item.voucher_code)
             })
--- src/views/works/travel/newProcess.vue
+++ src/views/works/travel/newProcess.vue
@@ -736,7 +736,7 @@
                     </template>
                   </el-table-column>
                   <el-table-column
-                    prop="oad04"
+                    prop="oad04_show"
                     label="借款人"
                     min-width="110px"
                     align="center"
@@ -744,7 +744,7 @@
                     <template slot-scope="scope">
                       <div>
                         <el-input
-                          v-model="scope.row.oad04"
+                          v-model="scope.row.oad04_show"
                           placeholder=""
                           disabled
                         ></el-input>
@@ -1443,6 +1443,7 @@ export default {
               this.$set(item,'oad02','')
               this.$set(item,'oad03',item.date)
               this.$set(item,'oad04',item.rid)
+              this.$set(item,'oad04_show',item.rname)
               this.$set(item,'oad05',item.original_amount)
               this.$set(item,'oad06',item.voucher_code)
             })
