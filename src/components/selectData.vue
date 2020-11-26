a50291aa7fcd3d368cdfa63aa24de913c71983e9
侯鑫 <houxin@qq.com>
2020-11-26 11:35:01 +0800

    修改弹框

--- src/components/selectData.vue
+++ src/components/selectData.vue
@@ -263,9 +263,14 @@ export default {
     },
     // 清空时重新搜索所有值
     search_all() {
+      this.showLoading = true;
       request.get(this.searchApi, {}, {}).then((res) => {
         if (res.status == 200) {
+          this.showLoading = false;
           this.tableData = res.data;
+        }else{
+          this.showLoading = false;
+          this.$message.error('服务器错误！')
         }
       });
     },
