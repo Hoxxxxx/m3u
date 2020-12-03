d405c49b24f2cb41c8a8aaf65c0e5d09b2f32f18
侯鑫 <houxin@qq.com>
2020-12-03 13:24:53 +0800

    debug

--- src/views/works/invoice/newApproval.vue
+++ src/views/works/invoice/newApproval.vue
@@ -1250,7 +1250,7 @@ export default {
   },
   created() {
     this.workid = this.$route.query.workid;
-    this.workid = 4596
+    // this.workid = 4596
     this.getworkflows();
     this.getAzi(); //币种列表
     this.getPma(); //支付方式
