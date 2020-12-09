42a41d86e24257188a8f69ef8e9b0aeecb4ff912
侯鑫 <houxin@qq.com>
2020-12-09 12:33:41 +0800

    debug

--- src/views/works/invoice/newApproval.vue
+++ src/views/works/invoice/newApproval.vue
@@ -1498,7 +1498,7 @@ export default {
         return prev + Number(cur.oab05);
       }, 0);
       let sums = fomatFloat((sum*(1 + this.showData.oaa13_rate/100).toFixed(2)),2)
-      if (Number(this.tableData.oaa12) != sums) {
+      if (Number(this.tableData.oaa12) != (sums+sum)) {
         this.$message.warning("总金额有错误，请重新填写！");
       } else {
         editFlow(this.addParams).then((result) => {
@@ -1544,7 +1544,7 @@ export default {
         return prev + Number(cur.oab05);
       }, 0);
       let sums = fomatFloat((sum*(1 + this.showData.oaa13_rate/100).toFixed(2)),2)
-      if (Number(this.tableData.oaa12) != sums) {
+      if (Number(this.tableData.oaa12) != (sums+sum)) {
         this.$message.warning("总金额有错误，请重新填写！");
       } else {
         editFlow(this.addParams).then((result) => {
