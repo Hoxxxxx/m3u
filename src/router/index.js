4b13c3b49e83edf5b0e722ae2b33999c8c4c1c0c
侯鑫 <houxin@qq.com>
2020-12-21 13:58:14 +0800

    sso优化

--- src/router/index.js
+++ src/router/index.js
@@ -419,7 +419,7 @@ router.beforeEach((to, from, next) => {
           sessionStorage.setItem('exp',code.exp)
           sessionStorage.setItem('oauserinfo',JSON.stringify(oauserinfo))
           // sessionStorage.setItem('OrgId', code.orgid)
-          sessionStorage.setItem('OrgId', allParams.orgid)
+          sessionStorage.setItem('OrgId', allParams.orgid ? allParams.orgid : code.orgid)
           sessionStorage.setItem('token', token)
           delete allParams.code 
           next({
