ab434ef1a92634eb6c15fc59aaa09ef44cfd9e64
侯鑫 <houxin@qq.com>
2021-01-19 17:09:42 +0800

    debug

--- src/store/state.js
+++ src/store/state.js
@@ -1,11 +1,17 @@
 let workInfo = sessionStorage.getItem('workInfo') ? JSON.parse(sessionStorage.getItem('workInfo')) : {}
 let userInfo = sessionStorage.getItem('userInfo') ? JSON.parse(sessionStorage.getItem('userInfo')) : {}
 let companyInfo = sessionStorage.getItem('companyInfo') ? JSON.parse(sessionStorage.getItem('companyInfo')) : {}
+let orgid = sessionStorage.getItem('OrgId') ? JSON.parse(sessionStorage.getItem('OrgId')) : 2
 const state = {
   workInfo: workInfo,
   userInfo: userInfo,
   companyInfo: companyInfo,
   upload_url: `${process.env.VUE_APP_BASE_API}files/upload`,
   form:{},//自定义表单数据
+  filter_tplid:{
+    FILTER_GDZCSQD: process.env.NODE_ENV == 'development' ? '8946': (orgid == 1 ? '8910' : (orgid == 2 ? '8952' : '8924')),//固定资产申请单
+    FILTER_CCSQD: process.env.NODE_ENV == 'development' ? '8941': (orgid == 1 ? '8906' : (orgid == 2 ? '8948' : '8920')),//出差申请单
+    FILTER_FKSQD: process.env.NODE_ENV == 'development' ? '8950': (orgid == 1 ? '8914' : (orgid == 2 ? '8956' : '8928')),//付款申请单
+  }
 }
 export default state
