9c269cd853015ed21e0aa6276f3a74dfaf4ca19a
侯鑫 <houxin@qq.com>
2021-01-30 17:52:50 +0800

    h5页面修改

--- src/views/h5/general.vue
+++ src/views/h5/general.vue
@@ -4,7 +4,6 @@
       <div class="head">
         <van-nav-bar
           :title="formData.work_name"
-          left-text="返回"
           left-arrow
           @click-left="onClickLeft"
         />
